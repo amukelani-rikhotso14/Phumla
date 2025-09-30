b9a9368d572bce5de48da028918676baa834fea2
Amukelani Rikhotso <168629631+amukelani-rikhotso14@users.noreply.github.com>
2025-09-30 13:40:03 +0200

    changes

--- Phumla/Program.cs
+++ Phumla/Program.cs
@@ -18,7 +18,7 @@ namespace Phumla
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new GuestLookupForm());
         }
     }
 }
